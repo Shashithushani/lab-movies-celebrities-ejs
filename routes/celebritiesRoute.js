1f878072c0f07c5b8059966ad6463521dd5df7fa
Shashika Samaradeera <thushanishashika23@gmail.com>
2023-07-11 13:54:38 +0200

    done

--- routes/celebritiesRoute.js
+++ routes/celebritiesRoute.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 // const { request } = require("../app");
 const Celebrity = require("../models/Celebrity.model");
 
-router.get("/celebrities/create", (req, res, next) => {
+router.get("/celebrities/create", (req, res) => {
   res.render("celebrities/new-celebrity");
 });
 
-router.post("/celebrities/create", async (req, res, next) => {
+router.post("/celebrities/create", async (req, res) => {
   console.log(req.body);
   const data = req.body;
   try {
@@ -18,10 +18,10 @@ router.post("/celebrities/create", async (req, res, next) => {
     console.log(error);
   }
 });
-router.get("/celebrities", async (req, res, next) => {
+router.get("/celebrities", async (req, res) => {
   try {
     const celeb = await Celebrity.find();
-    res.render("celebrities/celebrities.ejs", [celeb]);
+    res.render("celebrities/celebrities.ejs", { celeb });
   } catch (error) {
     console.log(error);
   }
